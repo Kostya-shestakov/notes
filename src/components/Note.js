a425de4b34df118eb9882fffe21455e973e66d33
kostya-dev <bezdusnyjkosta3@gmail.com>
2021-10-09 21:27:21 +0500

    commit

---
+++ src/components/Note.js
@@ -0,0 +1,17 @@
+
+
+const Note = ({ id, text, date, handleDeleteNote }) => {
+	return (
+		<div className='note'>
+			<span>{text}</span>
+			<div className='note-footer'>
+				<small>{date}</small>
+				<button onClick={() => handleDeleteNote(id)}
+					className='delete-icon'
+					size='1.3em'>delete</button>
+			</div>
+		</div>
+	);
+};
+
+export default Note;
